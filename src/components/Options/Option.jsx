4ef8bf322c6c1dc688a865a4973b29c74feffd3c
jazzrebelbandoleros <lizatimoshenko123@gmail.com>
2024-10-07 17:41:07 +0300

    change all

--- src/components/Options/Option.jsx
+++ src/components/Options/Option.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import styles from "./Options.module.css";
+import styles from "./Option.module.css";
 import { useState } from "react";
 
 const Options = ({ updateFeedback, resetFeedback, totalFeedback }) => {
